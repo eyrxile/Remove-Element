8b074befadc99e9605dc2e4821c105c3325820ba
Eyrxile <91099446+eyrxile@users.noreply.github.com>
2024-03-16 13:07:16 -0300

    Add files via upload

---
+++ Remove Element.js
@@ -0,0 +1,23 @@
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] !== val){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
+//Example usage
+let nums1 = [3,2,2,3];
+let val1 = 3;
+console.log(removeElement(nums1, val1));
+console.log(nums1);
+
+let nums2 = [0,1,2,2,3,0,4,2];
+let val2 = 2;
+console.log(removeElement(nums2,val2));
+console.log(nums2);
